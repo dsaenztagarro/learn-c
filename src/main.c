c8c250ce6e5fdb0c377f472fe91a5ddff8f0ad7c
David Sáenz <dsaenz@bebanjo.com>
2025-04-12 16:55:35 +0200

    Factorial example

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+unsigned int factorial(unsigned int n)
+{                        // ^ qualifier for `int` and `char`
+    if (n == 0)
+        return 1;
+
+    return n * factorial(n -1);
+}
+
 int main(int argc, char **argv)
 {
-        printf("hello world\n");
+    unsigned int loop = 50;
+    while (loop--) {
+        printf("Factorial of number %d is %u\n",
+                loop,
+                factorial(loop));
+    }
+    return 0;
 }
